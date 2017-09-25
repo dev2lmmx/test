d6546477d35a20025b8ca2965f59301104d7e394
Pavel Talashchuk <dev2.lmmx.dev@gmail.com>
2017-09-25 12:03:03 +0300

    test

---
+++ app/src/main/java/com/example/dev2/testproject/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.dev2.testproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        print("Hello");
+    }
+
+    private void print(String text) {
+        System.out.println(text);
+    }
+}
